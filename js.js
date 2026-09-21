9bc8e72a9012ef2c5002c9253712df37bf759d52
stefangrage <hej@stefangrage.dk>
2026-09-21 23:21:05 +0200

    Tilføjer fixed menu

--- js.js
+++ js.js
@@ -1,8 +1,17 @@
 /* Mobil Menu */
     const navicon = document.querySelector('.navicon');
     const mobileMenu = document.querySelector('.mobile-menu');
+    const logo = document.querySelector('.logo');
 
-    navicon.addEventListener('click', () => {
+    const toggleMobileMenu = () => {
         mobileMenu.classList.toggle('hidden');
         mobileMenu.classList.toggle('flex');
+    };
+
+    navicon.addEventListener('click', toggleMobileMenu);
+    mobileMenu.addEventListener('click', toggleMobileMenu);
+    logo.addEventListener('click', () => {
+        if (mobileMenu.classList.contains('flex')) {
+            toggleMobileMenu();
+        }
     });
